86a2344cda04e6eb914c750428160e4aef34fdb0
Kamelof <artur.evstafev.00@gmail.com>
2021-11-02 12:14:55 +0200

    Add project files.

---
+++ SingalRChatClient/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace SingalRChatClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StartChatAsync().ConfigureAwait(false).GetAwaiter();
+            Console.ReadKey();
+        }
+
+        static async Task StartChatAsync()
+        {
+            HubConnection connection
+               = new HubConnectionBuilder()
+               .WithUrl("https://localhost:5001/chat")
+               .Build();
+
+            connection.On<string, string>("ReceiveMessage",
+                (user, message) =>
+                {
+                    var newMessage = $"{user}: {message}";
+                    Console.WriteLine(newMessage);
+                });
+
+            await connection.StartAsync();
+
+            string message;
+            do
+            {
+                message = Console.ReadLine();
+                await connection.InvokeAsync("SendMessage",
+                      "ITEA", message);
+            } while (!string.IsNullOrEmpty(message));
+        }
+    }
+}
